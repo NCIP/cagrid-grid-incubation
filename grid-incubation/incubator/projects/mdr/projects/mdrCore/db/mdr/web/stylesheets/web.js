6e021dc854a087fe32744da7cbd953d9d09c73cd
mathurp <mathurp>
2010-01-31 23:28:05 +0000

    added form validation func: validate_adminItems

--- grid-incubation/incubator/projects/mdr/projects/mdrCore/db/mdr/web/stylesheets/web.js
+++ grid-incubation/incubator/projects/mdr/projects/mdrCore/db/mdr/web/stylesheets/web.js
@@ -40,3 +40,62 @@ function changeLetter(letter)
       form.start.value = 1;
       form.submit();
     }
+    
+function validate_adminItems ()
+{
+    valid = true;
+    errors = new Array();
+    i=0;
+ 
+    regauth = document.getElementById('registration-authority');
+    regby = document.getElementById('registered-by');
+    adminby = document.getElementById('administered-by');
+    submitby = document.getElementById('submitted-by');
+    adminstatus = document.getElementById('administrative-status');
+    
+    if (regauth.options[regauth.selectedIndex].value == "")
+    { 
+        errors[i] = "Registration Authority. " ;
+        i = i+1;
+        valid = false;
+        
+    }
+    
+    if (regby.options[regby.selectedIndex].value == "")
+    { 
+        errors[i] = "Registered By. " ;
+        i++;
+    }
+
+    if (adminby.options[adminby.selectedIndex].value == "")
+    { 
+        errors[i] = "Administered By. " ;
+        i++;
+    }
+    
+    if (submitby.options[submitby.selectedIndex].value == "")
+    { 
+        errors[i] = "Submitted By. " ;
+        i++;
+    }
+
+    if (adminstatus.options[adminstatus.selectedIndex].value == "")
+    { 
+        errors[i] = "Administrative Status. " ;
+        i++;
+    }
+    
+    if(errors.length>0)
+    {
+        errMsg = "Please select the following Administered items :- \n ";
+        for(j=0; j<errors.length; j++){
+            errMsg = errMsg + "\n" + errors[j];
+        }
+        alert(errMsg);
+        valid = false;
+    }    
+    
+    return valid;
+}
+
+
