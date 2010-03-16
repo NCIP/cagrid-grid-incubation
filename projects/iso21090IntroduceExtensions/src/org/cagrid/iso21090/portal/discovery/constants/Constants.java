54b2a938627b80b4811a8aa1d25c603bfb5e0a18
oster <oster>
2010-03-16 19:19:21 +0000

    first cut of "working" extension modified service still doesn't work, as the detail of the serialization need to be addressed, but the extension is adding the correct things to the service

--- projects/iso21090IntroduceExtensions/src/org/cagrid/iso21090/portal/discovery/constants/Constants.java
+++ projects/iso21090IntroduceExtensions/src/org/cagrid/iso21090/portal/discovery/constants/Constants.java
@@ -6,7 +6,12 @@ public interface Constants {
 
     public static final String DATATYPES_NAMESPACE_KEY = "ISO21090_DATATYPES_NAMESPACE";
     public static final String DATATYPES_PACKAGE_KEY = "ISO21090_DATATYPES_PACKAGE";
+    public static final String DATATYPES_FILENAME_KEY = "ISO21090_DATATYPES_FILENAME";
+
     public static final String EXTENSION_NAMESPACE_KEY = "ISO21090_EXTENSION_NAMESPACE";
     public static final String EXTENSION_PACKAGE_KEY = "ISO21090_EXTENSION_PACKAGE";
+    public static final String EXTENSION_FILENAME_KEY = "ISO21090_EXTENSION_FILENAME";
 
+    public static final String DESERIALIZER_FACTORY_CLASSNAME = "gov.nih.nci.iso21090.grid.ser.JaxbDeserializerFactory";
+    public static final String SERIALIZER_FACTORY_CLASSNAME = "gov.nih.nci.iso21090.grid.ser.JaxbSerializerFactory";
 }
