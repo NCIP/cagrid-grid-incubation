d6f374fc3c44744708ec7ab7e1632a892ae6bc87
oster <oster>
2010-03-16 19:19:21 +0000

    first cut of "working" extension modified service still doesn't work, as the detail of the serialization need to be addressed, but the extension is adding the correct things to the service

--- grid-incubation/incubator/projects/iso21090/projects/iso21090IntroduceExtensions/src/org/cagrid/iso21090/portal/discovery/ISO21090TypeSelectionComponent.java
+++ grid-incubation/incubator/projects/iso21090/projects/iso21090IntroduceExtensions/src/org/cagrid/iso21090/portal/discovery/ISO21090TypeSelectionComponent.java
@@ -6,75 +6,210 @@ import gov.nih.nci.cagrid.introduce.beans.configuration.NamespaceReplacementPoli
 import gov.nih.nci.cagrid.introduce.beans.extension.DiscoveryExtensionDescriptionType;
 import gov.nih.nci.cagrid.introduce.beans.namespace.NamespaceType;
 import gov.nih.nci.cagrid.introduce.beans.namespace.NamespacesType;
-import gov.nih.nci.cagrid.introduce.common.FileFilters;
+import gov.nih.nci.cagrid.introduce.beans.namespace.SchemaElementType;
+import gov.nih.nci.cagrid.introduce.common.CommonTools;
+import gov.nih.nci.cagrid.introduce.extension.ExtensionTools;
 import gov.nih.nci.cagrid.introduce.extension.ExtensionsLoader;
+import gov.nih.nci.cagrid.introduce.extension.utils.ExtensionUtilities;
 import gov.nih.nci.cagrid.introduce.portal.modification.discovery.NamespaceTypeDiscoveryComponent;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
-import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.cagrid.iso21090.portal.discovery.constants.Constants;
 
 
+@SuppressWarnings("serial")
 public class ISO21090TypeSelectionComponent extends NamespaceTypeDiscoveryComponent {
+    private static final Log LOG = LogFactory.getLog(ISO21090TypeSelectionComponent.class);
+
 
     public ISO21090TypeSelectionComponent(DiscoveryExtensionDescriptionType descriptor, NamespacesType currentNamespaces) {
         super(descriptor, currentNamespaces);
-        // TODO Auto-generated constructor stub
+    }
+
+
+    protected String getISONamespace() {
+        return ExtensionTools.getProperty(getDescriptor().getProperties(), Constants.DATATYPES_NAMESPACE_KEY);
+    }
+
+
+    protected String getISOXSDFilename() {
+        return ExtensionTools.getProperty(getDescriptor().getProperties(), Constants.DATATYPES_FILENAME_KEY);
+    }
+
+
+    protected String getISOPackage() {
+        return ExtensionTools.getProperty(getDescriptor().getProperties(), Constants.DATATYPES_PACKAGE_KEY);
+    }
+
+
+    protected String getISOExtensionsNamespace() {
+        return ExtensionTools.getProperty(getDescriptor().getProperties(), Constants.EXTENSION_NAMESPACE_KEY);
+    }
+
+
+    protected String getISOExtensionsXSDFilename() {
+        return ExtensionTools.getProperty(getDescriptor().getProperties(), Constants.EXTENSION_FILENAME_KEY);
+    }
+
+
+    protected String getISOExtensionsPackage() {
+        return ExtensionTools.getProperty(getDescriptor().getProperties(), Constants.EXTENSION_PACKAGE_KEY);
     }
 
 
     @Override
-    public NamespaceType[] createNamespaceType(File schemaDir, NamespaceReplacementPolicy arg1,
-        MultiEventProgressBar arg2) {
+    public NamespaceType[] createNamespaceType(File schemaDir, NamespaceReplacementPolicy replacementPolicy,
+        MultiEventProgressBar progress) {
 
         // check the namespace replacement policy and see what to do if the
         // stuff we plan to add already exists
-        // if (replacementPolicy.equals(NamespaceReplacementPolicy.IGNORE)) {
+        if (namespaceAlreadyExists(getISONamespace())) {
+            if (replacementPolicy.equals(NamespaceReplacementPolicy.ERROR)) {
+                String error = "Namespace ("
+                    + getISONamespace()
+                    + ") already exists, and policy was to error. Change the setting in the Preferences to REPLACE or IGNORE to avoid this error.";
+                LOG.error(error);
+                addError(error);
+                return null;
+            } else {
+                LOG.info("The ISO21090 Datatype schema already exists, the non-ERROR policy is:"
+                    + replacementPolicy.getValue());
+            }
+        }
+        if (namespaceAlreadyExists(getISOExtensionsNamespace())) {
+            if (replacementPolicy.equals(NamespaceReplacementPolicy.ERROR)) {
+                String error = "Namespace ("
+                    + getISOExtensionsNamespace()
+                    + ") already exists, and policy was to error. Change the setting in the Preferences to REPLACE or IGNORE to avoid this error.";
+                LOG.error(error);
+                addError(error);
+                return null;
+            } else {
+                LOG.info("The ISO21090 Extensions schema already exists, the non-ERROR policy is:"
+                    + replacementPolicy.getValue());
+            }
+        }
 
         // copy the schemas
-        
+        File copiedISOXSDFilename = null;
+        File copiedISOExtensionsXSDFilename = null;
         try {
-            copyISOSchemas(schemaDir);
+            copiedISOXSDFilename = copySchemaFromExtensionDir(getISOXSDFilename(), schemaDir);
+            copiedISOExtensionsXSDFilename = copySchemaFromExtensionDir(getISOExtensionsXSDFilename(), schemaDir);
         } catch (IOException e) {
             addError("Problem copying schemas:" + e.getMessage());
             setErrorCauseThrowable(e);
             return null;
         }
 
+        // copy the jar files and fix classpath
+        try {
+            copyLibraries(getServiceDirectory(schemaDir));
+        } catch (Exception e) {
+            addError("Problem copying jar files:" + e.getMessage());
+            setErrorCauseThrowable(e);
+            return null;
+        }
+
+        NamespaceType[] createdTypes = new NamespaceType[2];
         // create the namespace types with commontools
-        //  <ns2:Namespace location="./ISO_datatypes_Narrative.xsd" namespace="uri:iso.org:21090" packageName="org.iso._21090" xsi:type="ns2:NamespaceType">
-        //  <ns2:Namespace generateStubs="false" location="./ISO_extensions.xsd" namespace="http://iso21090.nci.nih.gov" packageName="gov.nih.nci.iso21090.extensions" xsi:type="ns2:NamespaceType">
-        //   <ns2:SchemaElement className="Bl" deserializer="gov.nih.nci.iso21090.grid.ser.JaxbDeserializerFactory" serializer="gov.nih.nci.iso21090.grid.ser.JaxbSerializerFactory" type="Bl" xsi:type="ns2:SchemaElementType"/>
+        try {
+            createdTypes[0] = CommonTools.createNamespaceType(copiedISOXSDFilename.getAbsolutePath(), schemaDir);
+            createdTypes[0].setGenerateStubs(false);
+            createdTypes[0].setPackageName(getISOPackage());
+
+            createdTypes[1] = CommonTools.createNamespaceType(copiedISOExtensionsXSDFilename.getAbsolutePath(),
+                schemaDir);
+            createdTypes[1].setGenerateStubs(false);
+            createdTypes[1].setPackageName(getISOExtensionsPackage());
+        } catch (Exception e) {
+            addError("Problem creating namespace types:" + e.getMessage());
+            setErrorCauseThrowable(e);
+            return null;
+        }
 
         // walk thru them and configure the serializers
-
-        return null;
-
-    }
-
-
-    private void copyISOSchemas(File schemaDir) throws IOException {
-        System.out.println("Copying schemas to " + schemaDir);
-        File extensionSchemaDir = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator
-            + Constants.EXTENSION_NAME + File.separator + "schema");
-        List<File> schemaFiles = Utils.recursiveListFiles(extensionSchemaDir, new FileFilters.XSDFileFilter());
-        for (File schemaFile : schemaFiles) {
-            String subname = schemaFile.getCanonicalPath().substring(
-                extensionSchemaDir.getCanonicalPath().length() + File.separator.length());
-            copySchema(subname, schemaDir);
+        // TODO: should both use the same framework? The reference service just
+        // configures the extension schema
+        for (SchemaElementType se : createdTypes[0].getSchemaElement()) {
+            // TODO is this a valid assumption for all the types? If not, need a
+            // mapping file
+            se.setClassName(se.getType());
+            se.setDeserializer(Constants.DESERIALIZER_FACTORY_CLASSNAME);
+            se.setSerializer(Constants.SERIALIZER_FACTORY_CLASSNAME);
         }
+        for (SchemaElementType se : createdTypes[1].getSchemaElement()) {
+            // TODO is this a valid assumption for all the types? If not, need a
+            // mapping file
+            se.setClassName(se.getType());
+            se.setDeserializer(Constants.DESERIALIZER_FACTORY_CLASSNAME);
+            se.setSerializer(Constants.SERIALIZER_FACTORY_CLASSNAME);
+        }
+
+        return createdTypes;
+
     }
 
 
-    private void copySchema(String schemaName, File outputDir) throws IOException {
+    /**
+     * HACK: This should be done more elegantly but is limited by introduce as
+     * described in this RFE:
+     * http://gforge.nci.nih.gov/tracker/?func=detail&group_id
+     * =25&aid=21615&atid=2252
+     * 
+     * @param schemaDir
+     *            The provided schema directory of the service
+     * @return The directory of the service
+     */
+    protected File getServiceDirectory(File schemaDir) {
+        return new File(schemaDir + "/../..");
+    }
+
+
+    protected File copySchemaFromExtensionDir(String schemaName, File outputDir) throws IOException {
         File schemaFile = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator + Constants.EXTENSION_NAME
             + File.separator + "schema" + File.separator + schemaName);
-        System.out.println("Copying schema from " + schemaFile.getAbsolutePath());
+        LOG.debug("Copying schema from " + schemaFile.getAbsolutePath());
         File outputFile = new File(outputDir + File.separator + schemaName);
-        System.out.println("Saving schema to " + outputFile.getAbsolutePath());
+        LOG.debug("Saving schema to " + outputFile.getAbsolutePath());
         Utils.copyFile(schemaFile, outputFile);
+        return outputFile;
+    }
+
+
+    protected void copyLibraries(File serviceDirectory) throws Exception {
+        // from the lib directory
+        File libDir = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator + Constants.EXTENSION_NAME
+            + File.separator + "lib");
+        File[] libs = libDir.listFiles(new FileFilter() {
+            public boolean accept(File pathname) {
+                String name = pathname.getName();
+                // take any jar from our extension's directory
+                return (name.endsWith(".jar"));
+            }
+        });
+
+        if (libs != null) {
+            File[] copiedLibs = new File[libs.length];
+            for (int i = 0; i < libs.length; i++) {
+                File outFile = new File(serviceDirectory + File.separator + "lib" + File.separator + libs[i].getName());
+                copiedLibs[i] = outFile;
+                Utils.copyFile(libs[i], outFile);
+            }
+            modifyClasspathFile(copiedLibs, serviceDirectory);
+        }
+
+    }
+
+
+    protected void modifyClasspathFile(File[] libs, File serviceDirectory) throws Exception {
+        File classpathFile = new File(serviceDirectory, ".classpath");
+        ExtensionUtilities.syncEclipseClasspath(classpathFile, libs);
     }
 
 }
