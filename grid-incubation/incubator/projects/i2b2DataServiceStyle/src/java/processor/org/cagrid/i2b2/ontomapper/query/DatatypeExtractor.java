63ad0d47e1f3a1c6a8d27ec4b37d1a71c32a1b33
dervin <dervin>
2010-04-06 15:14:11 +0000

    TRIAD-333

--- grid-incubation/incubator/projects/i2b2DataServiceStyle/src/java/processor/org/cagrid/i2b2/ontomapper/query/DatatypeExtractor.java
+++ grid-incubation/incubator/projects/i2b2DataServiceStyle/src/java/processor/org/cagrid/i2b2/ontomapper/query/DatatypeExtractor.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.cagrid.i2b2.domain.Concept;
@@ -13,9 +14,6 @@ import org.cagrid.i2b2.ontomapper.processor.DatabaseConnectionSource;
 
 public class DatatypeExtractor {
     
-    // placeholder for table name prefixes in queries
-    private static final String PREFIX = "<!--TablePrefix-->";
-
     private DatabaseConnectionSource connectionSource = null;
     private String tablePrefix = null;
     
@@ -28,10 +26,27 @@ public class DatatypeExtractor {
     
     public List<Concept> getConcepts(List<QueryColumnCriteria> criteria) throws SQLException {
         List<Concept> concepts = new ArrayList<Concept>();
+        // get the base query and a blank list of parameters
+        String query = Queries.getConceptQuery(tablePrefix);
+        List<Object> parameters = new ArrayList<Object>();
+        if (criteria != null && criteria.size() != 0) {
+            // if some criteria are to be applied, 
+            // do it with a where clause and positional parameters
+            ParameterizedSqlFragment whereClause = criteriaToClause(criteria, "AND");
+            query = query + " where " + whereClause.getSql();
+            parameters = whereClause.getParameters();
+        }
+        // get a connection
         Connection connection = connectionSource.getConnection();
-        String query = replacePrefixes(Queries.CONCEPT_QUERY);
+        // prepare a statement w/ positional parameters
         PreparedStatement statement = connection.prepareStatement(query);
+        // set all the positional parameters
+        for (int i = 0; i < parameters.size(); i++) {
+            statement.setObject(i + 1, parameters.get(i));
+        }
+        // execute
         ResultSet results = statement.executeQuery();
+        // build up the concept object from returned columns
         while(results.next()) {
             Concept concept = new Concept();
             concept.setConceptPath(results.getString(1));
@@ -43,15 +58,32 @@ public class DatatypeExtractor {
             concept.setEncodingServiceURL(results.getString(7));
             concepts.add(concept);
         }
+        // close out everything
+        results.close();
+        statement.close();
         connection.close();
+        // wrap the results as an unmodifiable list so users don't try
+        // to mess with the results and have unexpected things happen
         return Collections.unmodifiableList(concepts);
     }
     
     
-    private String replacePrefixes(String query) {
-        // if null prefix, replace with an empty string
-        String replacement = tablePrefix != null ? tablePrefix : "";
-        String editedQuery = query.replace(PREFIX, replacement);
-        return editedQuery;
+    private ParameterizedSqlFragment criteriaToClause(List<QueryColumnCriteria> criteria, String operator) {
+        StringBuffer sql = new StringBuffer();
+        List<Object> params = new ArrayList<Object>();
+        Iterator<QueryColumnCriteria> criteriaIter = criteria.iterator();
+        while (criteriaIter.hasNext()) {
+            QueryColumnCriteria c = criteriaIter.next();
+            if (tablePrefix != null) {
+                sql.append(tablePrefix);
+            }
+            sql.append(c.getTableName()).append(".").append(c.getColumnName())
+                .append(" ").append(c.getPredicate()).append(" ?");
+            params.add(c.getValue());
+            if (criteriaIter.hasNext()) {
+                sql.append(" ").append(operator).append(" ");
+            }
+        }
+        return new ParameterizedSqlFragment(sql.toString(), params);
     }
 }
--- grid-incubation/incubator/projects/i2b2DataServiceStyle/src/java/processor/org/cagrid/i2b2/ontomapper/query/Queries.java
+++ grid-incubation/incubator/projects/i2b2DataServiceStyle/src/java/processor/org/cagrid/i2b2/ontomapper/query/Queries.java
@@ -1,24 +1,40 @@
 package org.cagrid.i2b2.ontomapper.query;
 
-public interface Queries {
 
+public class Queries {
+    
+    // placeholder for table name prefixes in queries
+    private static final String PREFIX = "<!--TablePrefix-->";
+    
     public static final String CONCEPT_QUERY = 
         "SELECT " +
-        "<!--TablePrefix-->.CONCEPT_DIMENSION.CONCEPT_PATH," +
-        "<!--TablePrefix-->.CONCEPT_DIMENSION.NAME_CHAR," +
-        "<!--TablePrefix-->.ENCODING_DIMENSION.CDE_PUBLIC_ID," +
-        "<!--TablePrefix-->.ENCODING_DIMENSION.CDE_VERSION," +
-        "<!--TablePrefix-->.ENCODING_PROJECT.PROJECT_NAME," +
-        "<!--TablePrefix-->.ENCODING_PROJECT.PROJECT_VERSION," +
-        "<!--TablePrefix-->.ENCODING_SERVICE.SERVICE_URL " +
+        "<!--TablePrefix-->CONCEPT_DIMENSION.CONCEPT_PATH," +
+        "<!--TablePrefix-->CONCEPT_DIMENSION.NAME_CHAR," +
+        "<!--TablePrefix-->ENCODING_DIMENSION.CDE_PUBLIC_ID," +
+        "<!--TablePrefix-->ENCODING_DIMENSION.CDE_VERSION," +
+        "<!--TablePrefix-->ENCODING_PROJECT.PROJECT_NAME," +
+        "<!--TablePrefix-->ENCODING_PROJECT.PROJECT_VERSION," +
+        "<!--TablePrefix-->ENCODING_SERVICE.SERVICE_URL " +
         "FROM " +
-        "<!--TablePrefix-->.CONCEPT_DIMENSION " +
-        "INNER JOIN <!--TablePrefix-->.ENCODING_DIMENSION " +
-        "ON (<!--TablePrefix-->.CONCEPT_DIMENSION.ENCODING_CD = <!--TablePrefix-->.ENCODING_DIMENSION.ENCODING_CD) " +
-        "INNER JOIN <!--TablePrefix-->.ENCODING_PROJECT_LINK " +
-        "ON (<!--TablePrefix-->.ENCODING_DIMENSION.ENCODING_CD = <!--TablePrefix-->.ENCODING_PROJECT_LINK.ENCODING_CD) " +
-        "INNER JOIN <!--TablePrefix-->.ENCODING_PROJECT " +
-        "ON (<!--TablePrefix-->.ENCODING_PROJECT_LINK.ENCODING_PROJECT_ID = <!--TablePrefix-->.ENCODING_PROJECT.ENCODING_PROJECT_ID) " +
-        "INNER JOIN <!--TablePrefix-->.ENCODING_SERVICE " +
-        "ON (<!--TablePrefix-->.ENCODING_PROJECT.ENCODING_SERVICE_ID = <!--TablePrefix-->.ENCODING_SERVICE.ENCODING_SERVICE) ;";
+        "<!--TablePrefix-->CONCEPT_DIMENSION " +
+        "INNER JOIN <!--TablePrefix-->ENCODING_DIMENSION " +
+        "ON (<!--TablePrefix-->CONCEPT_DIMENSION.ENCODING_CD = <!--TablePrefix-->ENCODING_DIMENSION.ENCODING_CD) " +
+        "INNER JOIN <!--TablePrefix-->ENCODING_PROJECT_LINK " +
+        "ON (<!--TablePrefix-->ENCODING_DIMENSION.ENCODING_CD = <!--TablePrefix-->ENCODING_PROJECT_LINK.ENCODING_CD) " +
+        "INNER JOIN <!--TablePrefix-->ENCODING_PROJECT " +
+        "ON (<!--TablePrefix-->ENCODING_PROJECT_LINK.ENCODING_PROJECT_ID = <!--TablePrefix-->ENCODING_PROJECT.ENCODING_PROJECT_ID) " +
+        "INNER JOIN <!--TablePrefix-->ENCODING_SERVICE " +
+        "ON (<!--TablePrefix-->ENCODING_PROJECT.ENCODING_SERVICE_ID = <!--TablePrefix-->ENCODING_SERVICE.ENCODING_SERVICE) ;";
+    
+    public static String getConceptQuery(String tablePrefix) {
+        return replacePrefixes(CONCEPT_QUERY, tablePrefix);
+    }
+    
+    
+    private static String replacePrefixes(String query, String tablePrefix) {
+        // if null prefix, replace with an empty string
+        String replacement = tablePrefix != null ? tablePrefix : "";
+        String editedQuery = query.replace(PREFIX, replacement);
+        return editedQuery;
+    }
 }
