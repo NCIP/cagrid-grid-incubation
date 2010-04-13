a980145662abee09d8858fe6cea9e3c29426f3a7
dervin <dervin>
2010-04-13 18:45:58 +0000

    importing CQL to HQL translator from incubator

---
+++ projects/iso21090SdkQueryProcessor/src/java/translator/org/cagrid/iso21090/sdkquery/translator/TypesInformationResolver.java
@@ -0,0 +1,18 @@
+package org.cagrid.iso21090.sdkquery.translator;
+
+/**
+ * TypesInformationResolver
+ * Used to resolve various information about domain datatypes
+ * 
+ * @author David
+ */
+public interface TypesInformationResolver {
+
+    public Object getClassDiscriminatorValue(String classname) throws TypesInformationException;
+    
+    public boolean classHasSubclasses(String classname) throws TypesInformationException;
+    
+    public Class<?> getJavaDataType(String classname, String field) throws TypesInformationException;
+    
+    public String getRoleName(String parentClassname, String childClassname) throws TypesInformationException;
+}
