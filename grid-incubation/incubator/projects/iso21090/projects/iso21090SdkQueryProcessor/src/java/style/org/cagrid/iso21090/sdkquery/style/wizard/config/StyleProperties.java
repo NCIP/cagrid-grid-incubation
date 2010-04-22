7f3700749f9ea14fb153307f257b560c874f9ff4
dervin <dervin>
2010-04-22 15:46:56 +0000

    CAGRID-384: code to make the style go... still largely a copy of SDK 4.2 style, needs some tweaks

---
+++ grid-incubation/incubator/projects/iso21090/projects/iso21090SdkQueryProcessor/src/java/style/org/cagrid/iso21090/sdkquery/style/wizard/config/StyleProperties.java
@@ -0,0 +1,7 @@
+package org.cagrid.iso21090.sdkquery.style.wizard.config;
+
+public interface StyleProperties {
+
+    public static final String SDK_REMOTE_CLIENT_DIR = "sdk.remote.client.dir";
+    public static final String SDK_LOCAL_CLIENT_DIR = "sdk.local.client.dir";
+}
