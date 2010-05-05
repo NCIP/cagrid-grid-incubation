2d0ba054ba8d55ebbcb7496cbc071e9ddb3f9e46
lalkotas <lalkotas>
2010-05-05 17:37:13 +0000

    added service properties for authorization and mapping properties

--- grid-incubation/incubator/projects/Redcap/projects/RedcapDataService/src/org/cagrid/redcap/util/PropertiesUtil.java
+++ grid-incubation/incubator/projects/Redcap/projects/RedcapDataService/src/org/cagrid/redcap/util/PropertiesUtil.java
@@ -1,32 +1,52 @@
 package org.cagrid.redcap.util;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+/*
+ * Utility to get the properties from 
+ * the properties file.
+ */
 public class PropertiesUtil {
 
-	public static String getQuery(String key) {
-		InputStream inStream = PropertiesUtil.class
-				.getResourceAsStream("/rc_auth_queries.properties");
-		String query = loadProperties(key, inStream);
+	private static final Log LOG = LogFactory.getLog(PropertiesUtil.class);
+	
+	/*
+	 * Gets the required query from the properties
+	 * with the specified key.
+	 * @key: key in the properties file
+	 * @file: the location of the properties file(usually in etc)
+	 */
+	public static String getQuery(String key, String file) {
+		String query = loadProperties(key, file);
+		LOG.debug("Retrieving key["+key+"] from file ["+file+"] value: "+query);
 		return query;
 	}
 
-	public static String getUsernames(String key) {
-		InputStream inStream = PropertiesUtil.class
-				.getResourceAsStream("/rc_user_names.properties");
-		String value = loadProperties(key,inStream);
+	/*
+	 * Gets the required user name from the properties file
+	 * with the specified key to map Grid users with database users
+	 * @key: key in the properties file
+	 * @file: the location of the properties file(usually in etc)
+	 */
+	public static String getUsernames(String key,String file) {
+		String value = loadProperties(key,file);
+		LOG.debug("Retrieving key["+key+"] from file ["+file+"] value:"+value);
 		return value;
 	}
 
-	private static String loadProperties(String key, InputStream inputStream) {
+	private static String loadProperties(String key, String file) {
 		Properties props = new Properties();
 		String query = null;
 		try {
-			props.load(inputStream);
+			props.load(new FileInputStream(file));
 			query = props.getProperty(key);
 		} catch (IOException e) {
+			e.printStackTrace();
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return query;
