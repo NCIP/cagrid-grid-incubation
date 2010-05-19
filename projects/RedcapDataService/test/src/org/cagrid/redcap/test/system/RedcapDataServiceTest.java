bd76ac8ae6ced22ca6f5ec9d611085ca631d9ba0
lalkotas <lalkotas>
2010-05-19 17:36:13 +0000

    System tests for secured service.

--- projects/RedcapDataService/test/src/org/cagrid/redcap/test/system/RedcapDataServiceTest.java
+++ projects/RedcapDataService/test/src/org/cagrid/redcap/test/system/RedcapDataServiceTest.java
@@ -2,14 +2,16 @@ package org.cagrid.redcap.test.system;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.PropertyConfigurator;
 import org.cagrid.redcap.test.system.steps.InvokeClientStep;
+import org.cagrid.redcap.test.system.steps.RemoveAuthorizationStep;
 import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainer;
 import org.cagrid.redcap.test.system.steps.ConfigureRedcapDataServiceStep;
-import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainerFactory;
-import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainerType;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.CopyServiceStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.DeleteServiceStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.DeployServiceStep;
@@ -17,94 +19,104 @@ import gov.nih.nci.cagrid.testing.system.deployment.steps.DestroyContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.StartContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.StopContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.UnpackContainerStep;
-import gov.nih.nci.cagrid.testing.system.deployment.story.ServiceStoryBase;
+import gov.nih.nci.cagrid.testing.system.deployment.steps.CopyCAStep;
+import gov.nih.nci.cagrid.testing.system.deployment.SecureContainer;
+import gov.nih.nci.cagrid.testing.system.haste.Story;
 
-public class RedcapDataServiceTest extends ServiceStoryBase {
+public class RedcapDataServiceTest extends Story {
 
-	private RCDSTestCaseInfo tci = new RCDSTestCaseInfo();
 	private static final String NODEPLOYVALIDATION = "-Dno.deployment.validation=true";
 	private static final String NOINDEXREGISTRATION = "-Dperform.index.service.registration=false";
 	
+	private RCDSTestCaseInfo rcdsTestCaseInfo;
+	
+	private ServiceContainer container = null;
+	private String tmpDir;
+	File serviceDir = null;
+	
+	private static final Log LOG = LogFactory.getLog(RedcapDataServiceTest.class);
+	
 	public RedcapDataServiceTest(ServiceContainer container) {
-       super(container);
-       PropertyConfigurator.configure("." + File.separator + "test"+File.separator + "resources" + File.separator + "properties" + File.separator + "log4j.properties");
-    }
-    
-    public RedcapDataServiceTest() {
-        try {
-            this.setContainer(ServiceContainerFactory.createContainer(ServiceContainerType.TOMCAT_CONTAINER));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            fail("Failed to create container: " + ex.getMessage());
-        }
-    }
+       this.container=container;
+	}
 
     public String getName() {
         return getDescription();
     }
 
     public String getDescription() {
-        if(getContainer().getProperties().isSecure()){
-            return "Secure Transfer Service Test";
-        }
-        return "Transfer Service Test";
+        return "Redcap Data Service Test";
     }
 
-
+    protected boolean storySetUp() throws Throwable {
+    	
+    	boolean copyServiceStep = false;
+    	rcdsTestCaseInfo = new RCDSTestCaseInfo();
+    	tmpDir = rcdsTestCaseInfo.getTempDir(); 
+    	serviceDir = new File(rcdsTestCaseInfo.getTempDir());
+    	serviceDir.mkdirs();
+    	
+    	File srcDir = new File(System.getProperty("basedir"));
+    	if(serviceDir.exists() && serviceDir.isDirectory()){
+        	CopyServiceStep copyService = new CopyServiceStep(srcDir, serviceDir);
+    		try{
+    			copyService.runStep();
+    		}catch(Throwable e){
+    			e.printStackTrace();
+    		}
+    		copyServiceStep = true;
+        }
+    	if(copyServiceStep){
+    		PropertyConfigurator.configure(tmpDir + File.separator + "test/resources/properties/log4j.properties");
+    	}
+    	LOG.debug("Copied Service from ["+srcDir+"]to :"+serviceDir);
+    	return (container != null && copyServiceStep) ;
+    }
+    
     protected Vector<Object> steps() {
         Vector<Object> steps = new Vector<Object>();
         try {
-        	steps.add(new UnpackContainerStep(getContainer()));
-            System.out.println("skipping index service registration...................................");
-        	List<String> deploymentArgs = Arrays.asList(new String[] {NODEPLOYVALIDATION,NOINDEXREGISTRATION});
-            steps.add(new ConfigureRedcapDataServiceStep(tci,getContainer(),false));
-            steps.add(new DeployServiceStep(getContainer(), "tmp/RedcapDataService", deploymentArgs));
-            steps.add(new StartContainerStep(getContainer()));
-            steps.add(new InvokeClientStep(getContainer(), tci));
-            steps.add(new StopContainerStep(getContainer()));
+        	steps.add(new UnpackContainerStep(container));
+            steps.add(new CopyCAStep((SecureContainer) container, serviceDir));
+            steps.add(new RemoveAuthorizationStep(tmpDir,rcdsTestCaseInfo));
+            steps.add(new ConfigureRedcapDataServiceStep(rcdsTestCaseInfo,container,true));
+            List<String> deploymentArgs = Arrays.asList(new String[] {NODEPLOYVALIDATION,NOINDEXREGISTRATION});
+            steps.add(new DeployServiceStep(container, tmpDir, deploymentArgs));
+            steps.add(new StartContainerStep(container));
+            steps.add(new InvokeClientStep(container, rcdsTestCaseInfo));
         } catch (Exception e) {
             e.printStackTrace();
         }
         return steps;
     }
 
-    protected boolean storySetUp() throws Throwable {
-    	DeleteServiceStep step4 = new DeleteServiceStep(new File("tmp"));
-         try{
-         	step4.runStep();
-         }catch(Throwable e){
-         	e.printStackTrace();
-         }
-        File tempService = new File("tmp/RedcapDataService");
-		//File asLocation = new File("C:\\deletewrkspace\\RedcapDataService");
-        File asLocation = new File(".");
-		CopyServiceStep copyService = new CopyServiceStep(asLocation,tempService);
-		try{
-			copyService.runStep();
-		}catch(Throwable e){
-			e.printStackTrace();
-		}
-        return true;
-    }
-
     protected void storyTearDown() throws Throwable {
-    	DeleteServiceStep step4 = new DeleteServiceStep(new File("tmp/RedcapDataService"));
+		DeleteServiceStep step4 = new DeleteServiceStep(new File(rcdsTestCaseInfo.getTempDir()));
         try{
         	step4.runStep();
         }catch(Throwable e){
         	e.printStackTrace();
         }
-    	StopContainerStep step2 = new StopContainerStep(getContainer());
+    	List<Throwable> errors = new LinkedList<Throwable>();
+    	StopContainerStep step2 = new StopContainerStep(container);
         try {
             step2.runStep();
         } catch (Throwable e) {
-            e.printStackTrace();
+            errors.add(e);
         }
-        DestroyContainerStep step3 = new DestroyContainerStep(getContainer());
+        DestroyContainerStep step3 = new DestroyContainerStep(container);
         try {
             step3.runStep();
         } catch (Throwable e) {
-            e.printStackTrace();
+            errors.add(e);
+        }
+        if (errors.size() != 0) {
+            LOG.error("Exception(s) occured during TearDown:");
+            for (Throwable t : errors) {
+                System.err.println("----------------");
+                t.printStackTrace();
+            }
+            throw new Exception("Exception(s) occured during TearDown");
         }
     }
 }
--- projects/RedcapDataService/test/src/org/cagrid/redcap/test/system/steps/ConfigureRedcapDataServiceStep.java
+++ projects/RedcapDataService/test/src/org/cagrid/redcap/test/system/steps/ConfigureRedcapDataServiceStep.java
@@ -1,16 +1,20 @@
 package org.cagrid.redcap.test.system.steps;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
-import gov.nih.nci.cagrid.introduce.test.TestCaseInfo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cagrid.redcap.test.system.RCDSTestCaseInfo;
 import gov.nih.nci.cagrid.introduce.test.steps.BaseStep;
 import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainer;
 
 public class ConfigureRedcapDataServiceStep extends BaseStep{
 	
-	private TestCaseInfo tci;
+	private RCDSTestCaseInfo tci;
     private ServiceContainer container;
 	
     private static final String JDBCPWD = "cqlQueryProcessorConfig_jdbcPassword";
@@ -23,7 +27,9 @@ public class ConfigureRedcapDataServiceStep extends BaseStep{
 	private static final String DBPORT = "db.port";
 	private static final String DBSCHEMA = "db.schema.name";
     
-    public ConfigureRedcapDataServiceStep(TestCaseInfo tci, ServiceContainer container, boolean build)
+	private static final Log LOG = LogFactory.getLog(ConfigureRedcapDataServiceStep.class);
+	
+	public ConfigureRedcapDataServiceStep(RCDSTestCaseInfo tci, ServiceContainer container, boolean build)
 			throws Exception {
 		super(tci.getDir(), build);
         this.tci = tci;
@@ -31,10 +37,11 @@ public class ConfigureRedcapDataServiceStep extends BaseStep{
 	}
 
 	public void runStep() throws Throwable {
+		String path1 = System.getProperty("basedir")+File.separator+"test\\resources\\properties\\test.database.properties";
+		FileInputStream fis = new FileInputStream(path1);
 		
-		InputStream dbProps = this.getClass().getResourceAsStream("/properties/test.database.properties");
 		Properties dbProperties = new Properties();
-		dbProperties.load(dbProps);
+		dbProperties.load(fis);
 		
 		String dbPassword = dbProperties.getProperty(DBPWD);
 		String dbUsername = dbProperties.getProperty(DBUSER);
@@ -42,30 +49,25 @@ public class ConfigureRedcapDataServiceStep extends BaseStep{
 		String dbPort = dbProperties.getProperty(DBPORT);
 		String dbSchemaName = dbProperties.getProperty(DBSCHEMA);
 		String connectString = "jdbc:mysql://"+dbServer+":"+dbPort+"/"+dbSchemaName;
-		dbProps.close();
-	    
-		//String servicePropertiesFile =  "/"+ getBaseDir() + "/service.properties";
-		String servicePropertiesFile = "/service.properties";
-		System.out.println("printing service properties file"+servicePropertiesFile);
-		
-		
-		InputStream stream = this.getClass().getResourceAsStream(servicePropertiesFile);
+		String authorization=dbProperties.getProperty("authorization");
+				
+		String servicePropsLoc = tci.getTempDir()+File.separator+"service.properties";
+		FileInputStream stream = new FileInputStream(servicePropsLoc);
 		Properties props = new java.util.Properties();
 		props.load(stream);
 	 
 		props.setProperty(JDBCPWD, dbPassword);
 		props.setProperty(CONNECTSTRING, connectString);
 		props.setProperty(USERNAME, dbUsername);
-		 
+		props.setProperty("authorization", authorization); 
+		
+		LOG.debug("Configuring service with Authorization turned:"+authorization);
 		// Write properties file.
 		try {
-			String path = ".\\tmp\\"+getBaseDir()+"\\service.properties";
-			//props.store(new FileOutputStream(".\\tmp\\RedcapDataService\\service.properties"), null);
-			props.store(new FileOutputStream(path), null);
+			props.store(new FileOutputStream(servicePropsLoc), null);
 		} catch (IOException e) {
-		     System.out.println("cannot write to properties file::::::::"+e.getMessage());
+		     LOG.error("Error writing to properties file:"+e.getMessage());
 		}
 		stream.close();
-
 	}
 }
