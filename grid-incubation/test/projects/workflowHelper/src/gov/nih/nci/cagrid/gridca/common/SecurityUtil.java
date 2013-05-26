a566028021b1cac5608d4cbdc8b79ece94fb3ec3
John Eisenschmidt <je@lanternthree.com>
2013-05-26 15:40:15 -0300

    Update SecurityUtil.java

--- grid-incubation/test/projects/workflowHelper/src/gov/nih/nci/cagrid/gridca/common/SecurityUtil.java
+++ grid-incubation/test/projects/workflowHelper/src/gov/nih/nci/cagrid/gridca/common/SecurityUtil.java
@@ -1,8 +1,7 @@
 /**
 *============================================================================
-*  Copyright The Ohio State University Research Foundation, The University of Chicago - 
-*	Argonne National Laboratory, Emory University, SemanticBits LLC, and 
-*	Ekagra Software Technologies Ltd.
+*  Copyright (c) 2008, The Ohio State University Research Foundation, Emory 
+*  University, the University of Minnesota Supercomputing Institute
 *
 *  Distributed under the OSI-approved BSD 3-Clause License.
 *  See http://ncip.github.com/cagrid-core/LICENSE.txt for details.
